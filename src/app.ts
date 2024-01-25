4a2bf683f412e213827b378c628420f691acd9a5
developerMasum <developer.masum2001@gmail.com>
2024-01-25 10:14:30 +0600

    initial setup for ts-mongoose

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+
+// parsers
+// import cookieParser from 'cookie-parser';
+// app.use(cookieParser());
+
+app.use(express.json());
+
+// turn on cors true for redux Credential
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+// application routes
+// import router from './app/routes';
+// app.use('/api/v1', router);
+
+const test = async (req: Request, res: Response) => {
+  const a = "start,server running mama!!";
+  res.send(a.toString()); // Ensure sending a string
+};
+
+app.get("/", test);
+
+// import globalErrorHandler from './app/middlewares/globalErrorhandler';
+// app.use(globalErrorHandler);
+
+// import notFound from './app/middlewares/notFound';
+// app.use(notFound);
+
+export default app;
